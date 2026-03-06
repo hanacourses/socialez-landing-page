199cfc72fe4ca9d137de7b9221e35407de916338
Manii1997 <manibct1817@outlook.com>
2026-03-06 12:17:18 +0530

    cal.com script updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,48 +38,54 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen w-full min-w-0 antialiased bg-landing overflow-x-hidden snap-y snap-proximity`}
       >
-        <Script
-          id="cal-com-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function (C, A, L) { 
-                let p = function (a, ar) { a.q.push(ar); }; 
-                let d = C.document; 
-                C.Cal = C.Cal || function () { 
-                  let cal = C.Cal; 
-                  let ar = arguments; 
-                  if (!cal.loaded) { 
-                    cal.ns = {}; 
-                    cal.q = cal.q || []; 
-                    d.head.appendChild(d.createElement("script")).src = A; 
-                    cal.loaded = true; 
-                  } 
-                  if (ar[0] === L) { 
-                    const api = function () { p(api, arguments); }; 
-                    const namespace = ar[1]; 
-                    api.q = api.q || []; 
-                    if(typeof namespace === "string"){
-                      cal.ns[namespace] = cal.ns[namespace] || api;
-                      p(cal.ns[namespace], ar);
-                      p(cal, ["initNamespace", namespace]);
-                    } else p(cal, ar); 
-                    return;
-                  } 
-                  p(cal, ar); 
-                }; 
-              })(window, "https://app.cal.com/embed/embed.js", "init");
-              Cal("init", "socialez-demo", {origin:"https://app.cal.com"});
+        <Script id="cal-script" strategy="afterInteractive">
+        {`
+        (function (C, A, L) { 
+          let p = function (a, ar) { a.q.push(ar); }; 
+          let d = C.document; 
 
-              // Important: Please add the following attributes to the element that should trigger the calendar to open upon clicking.
-              // data-cal-link="bhaskarbaswala/socialez-demo"
-              // data-cal-namespace="socialez-demo"
-              // data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}'
+          C.Cal = C.Cal || function () { 
+            let cal = C.Cal; 
+            let ar = arguments; 
 
-              Cal.ns["socialez-demo"]("ui", {"cssVarsPerTheme":{"light":{"cal-brand":"#2B59AC"},"dark":{"cal-brand":"#4FCEFF"}},"hideEventTypeDetails":false,"layout":"month_view"});
-            `,
-          }}
-        />
+            if (!cal.loaded) { 
+              cal.ns = {}; 
+              cal.q = cal.q || []; 
+              d.head.appendChild(d.createElement("script")).src = A; 
+              cal.loaded = true; 
+            } 
+
+            if (ar[0] === L) { 
+              const api = function () { p(api, arguments); }; 
+              const namespace = ar[1]; 
+              api.q = api.q || []; 
+
+              if(typeof namespace === "string"){
+                cal.ns[namespace] = cal.ns[namespace] || api;
+                p(cal.ns[namespace], ar);
+                p(cal, ["initNamespace", namespace]);
+              } else p(cal, ar); 
+
+              return;
+            } 
+
+            p(cal, ar); 
+          }; 
+
+        })(window, "https://app.cal.com/embed/embed.js", "init");
+
+        Cal("init", "socialez-demo", {origin:"https://app.cal.com"});
+
+        Cal.ns["socialez-demo"]("ui", {
+          cssVarsPerTheme:{
+            light:{ "cal-brand":"#2B59AC"},
+            dark:{ "cal-brand":"#4FCEFF"}
+          },
+          hideEventTypeDetails:false,
+          layout:"month_view"
+        });
+        `}
+      </Script>
         <header className="fixed left-0 right-0 top-0 z-50 w-full px-4 pt-4 pb-2 sm:px-6">
           <Navbar />
         </header>
